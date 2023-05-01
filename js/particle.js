da5ffb36fc18b6116ca4e2bdf1f2f6ac38df9969
JingggZz <122839239+JingggZz@users.noreply.github.com>
2023-05-01 02:09:23 +0100

    Update particle.js

--- js/particle.js
+++ js/particle.js
@@ -1,7 +1,5 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/BjoM9oKOAKY
+// Main code from Daniel Shiffman
+// https://youtu.be/BjoM9oKOAKY
 
 class Particle {
   constructor() {
@@ -63,4 +61,62 @@ class Particle {
 
   }
 
+}
+
+//variables for flow field
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+
+var zoff = 0;
+
+var fr;
+
+var particles = [];
+
+var flowfield;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP('');
+
+  flowfield = new Array(cols * rows);
+
+  for (var i = 0; i < 500; i++) {
+    particles[i] = new Particle();
+  }
+
+  frameRate(60);
+  
+  background(186, 207, 225);
+}
+
+function draw() {
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(0, 50);
+    }
+    yoff += inc;
+
+    zoff += 0.0003;
+  }
+
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+
+  return;
 }
