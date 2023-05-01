6e428e69f99dfb4190245a2871cdf3cb2fb0189d
JingggZz <122839239+JingggZz@users.noreply.github.com>
2023-05-01 16:31:06 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 const questionText = document.querySelector(".question-text");
 const optionContainer = document.querySelector(".option-container");
+const finishedBox = document.querySelector(".finished-box");
+const answerIndicatorContainer = document.querySelector(".answer-indicator");
 
 let questionCounter = 0;
 let currentQuestions;
 let availableQuestions = [];
 let availableOptions = [];
+let answers = []; // added variable to store selected answers
 
 //push the queations into availableQueations Array
 function setAvailableQueations(){
@@ -41,34 +44,65 @@ function getNewQuestion(){
         option.id=i;
         option.className = "option"
         optionContainer.appendChild(option);
+        option.setAttribute("onclick", "getResult(this)");
+        
         // Add options to available options array
         availableOptions.push(result[i]);
     }
     questionCounter++
 }
 
+function answerIndicator(){
+    const totalQuestion = question.length;
+    for(let i=0; i<totalQuestion; i++){
+        const indicator = document.createElement("div");
+        indicator.innerHTML = answers[i];
+        answerIndicatorContainer.appendChild(indicator);
+    }
+
+    // show the question text and option container
+    questionText.style.display = 'block';
+    optionContainer.style.display = 'block';
+}
+
+function getResult(optionElement){
+    answers.push(optionElement.innerHTML); // add the selected option to the answers array
+    console.log(answers);
+}
+
+
 function next(){
     if(questionCounter === question.length){
-        console.log("question over");
+        answerIndicator();
+
+        // hide question text and option container
+        questionText.style.display = 'none';
+        optionContainer.style.display = 'none';
+
+        finishedBox.style.display = 'block';
+
     }else{
         getNewQuestion();
     }
 }
 
+
 function getRandomOptions(sourceArray, neededElements) {
     var result = [];
+
     // Make a copy of the sourceArray to avoid modifying the original one
     var optionsCopy = sourceArray.slice();
     for (var i = 0; i < neededElements; i++) {
         var index = Math.floor(Math.random() * optionsCopy.length)
         result.push(optionsCopy[index]);
+
         // Remove selected option from the copied array
         optionsCopy.splice(index, 1);
     }
     return result;
 }
 
-function getNewOptions(){
+function get(){
     
 }
 
@@ -77,4 +111,13 @@ window.onload = function(){
     setAvailableQueations();
     
     getNewQuestion();
+
+    // Add event listener to option container element
+    optionContainer.addEventListener('click', function(event) {
+    // Check if the clicked element is an option
+        if (event.target.classList.contains('option')) {
+        // Call next function to load next question
+            next();
+        }
+    });
 }
